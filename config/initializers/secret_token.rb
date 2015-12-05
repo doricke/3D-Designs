a0aeb7774dce90151618d35e6a349d026f487160
dricke <doricke@yahoo.com>
2015-12-05 14:41:22 -0500

    Easier startup

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,5 +9,5 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-MicroFluidics::Application.config.secret_key_base = ENV['SECRET_TOKEN']
-
+# MicroFluidics::Application.config.secret_key_base = ENV['SECRET_TOKEN']
+MicroFluidics::Application.config.secret_key_base = 'affd246274613a5d2fccfa8e544c3f97cc8029ae043e06d9aedf320bc915ce5a6cecb67fcf296b11f420257ad0139d28370c5067fc7def8e6ea52a5fec485c2d'
